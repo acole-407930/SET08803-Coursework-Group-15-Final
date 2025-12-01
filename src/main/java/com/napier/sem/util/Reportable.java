ffca2a5a9d7be388ce853a8e58233537a69cfdf9
acole-407930 <V12U@L_.3005.B1N0>
2025-11-30 18:31:44 -0600

    test 1

--- src/main/java/com/napier/sem/util/Reportable.java
+++ src/main/java/com/napier/sem/util/Reportable.java
@@ -27,7 +27,7 @@ public class Reportable {
             writer.write(sb.toString());
             writer.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); //test
         }
     }
 }
